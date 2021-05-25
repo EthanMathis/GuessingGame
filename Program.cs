c0c86d9359b5a5dade927b8068cf28bcda8420ff
EthanMathis <ethan.mathis@outlook.com>
2021-05-25 11:34:00 -0500

    phase 4 complete

--- Program.cs
+++ Program.cs
@@ -10,19 +10,49 @@ void Main()
 
 void Guess()
 {
-    Console.WriteLine("I'm thinking of a number.... Want to guess what it is?");
+    Console.WriteLine("Guess what number I'm thinking of. You get 4 tries.");
 
     string Guess = Console.ReadLine();
     int GuessNum = int.Parse(Guess);
 
     int SecretNumber = 42;
 
-    if (GuessNum == SecretNumber)
+    int Attempts = 0;
+
+    while (Attempts < 4)
     {
-        Console.WriteLine("THATS IT!!! How did you know?");
+        if (GuessNum == SecretNumber)
+        {
+            Console.WriteLine($"You guessed {GuessNum} > THATS IT!!! How did you know?");
+            Attempts = 4;
+        }
+        else if (Attempts == 3 && GuessNum != SecretNumber)
+        {
+            Console.WriteLine($"You guessed {GuessNum} > WRONG! You're all out of guesses");
+            Attempts++;
+        }
+        else
+        {
+            Console.WriteLine($"You guessed {GuessNum} > Not quite... Try again");
+            Guess = Console.ReadLine();
+            GuessNum = int.Parse(Guess);
+            Attempts++;
+        }
     }
-    else
-    {
-        Console.WriteLine("Nope. Not even close.");
-    }
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     if (GuessNum == SecretNumber)
+    //     {
+    //         Console.WriteLine("THATS IT!!! How did you know?");
+    //         i = 3;
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Nope. Try again");
+    //         Guess = Console.ReadLine();
+    //         GuessNum = int.Parse(Guess);
+    //     }
+    // }
+
 }
