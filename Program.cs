ebd5b06c8e1446f7d4f9ff050799c86eeeb7110c
EthanMathis <ethan.mathis@outlook.com>
2021-05-25 11:57:55 -0500

    phase 6 complete

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ void Main()
 
 void Guess()
 {
-    Console.WriteLine("Guess what number I'm thinking of. You get 4 tries.");
+    Console.WriteLine("I'm thinking of a number between 1 & 100. If you can guess it in 4 tries, I'll say somthing funny.");
 
     string Guess = Console.ReadLine();
     int GuessNum = int.Parse(Guess);
@@ -24,7 +24,7 @@ void Guess()
     {
         if (GuessNum == SecretNumber)
         {
-            Console.WriteLine($"You guessed {GuessNum} > THATS IT!!! How did you know?");
+            Console.WriteLine($"You guessed {GuessNum} > THATS IT!!! something funny....");
             Attempts = 0;
         }
         else if (Attempts == 1 && GuessNum != SecretNumber)
@@ -32,9 +32,17 @@ void Guess()
             Console.WriteLine($"You guessed {GuessNum} > WRONG! You're all out of guesses");
             Attempts--;
         }
+        else if (GuessNum < SecretNumber)
+        {
+            Console.WriteLine($"You guessed {GuessNum} > That's too low... Try again!");
+            Attempts--;
+            Console.WriteLine($"Number of guesses left: {Attempts}");
+            Guess = Console.ReadLine();
+            GuessNum = int.Parse(Guess);
+        }
         else
         {
-            Console.WriteLine($"You guessed {GuessNum} > Not quite... Try again");
+            Console.WriteLine($"You guessed {GuessNum} > That's too high... Try again!");
             Attempts--;
             Console.WriteLine($"Number of guesses left: {Attempts}");
             Guess = Console.ReadLine();
