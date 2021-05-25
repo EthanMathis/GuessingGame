27f5f341a3c62e69477af1e54870169a64d3fe1a
EthanMathis <ethan.mathis@outlook.com>
2021-05-25 14:37:00 -0500

    phase 8 complete | cheater mode added

--- Program.cs
+++ Program.cs
@@ -10,7 +10,11 @@ void Main()
 
 void Guess()
 {
-    Console.WriteLine("I'm thinking of a number between 1 & 100. If you can guess it in 4 tries, I'll say somthing funny.");
+    Console.WriteLine("I'm thinking of a number between 1 & 100. If you can guess it, I'll say somthing SUPER FUNNY!!");
+
+    int Attempts = Difficulty();
+
+    Console.WriteLine($"Alright, you get {Attempts} guesses. Go!");
 
     string Guess = Console.ReadLine();
     int GuessNum = int.Parse(Guess);
@@ -18,13 +22,13 @@ void Guess()
     Random r = new Random();
     int SecretNumber = r.Next(0, 100);
 
-    int Attempts = 4;
+    int Turn = 1;
 
     while (Attempts > 0)
     {
         if (GuessNum == SecretNumber)
         {
-            Console.WriteLine($"You guessed {GuessNum} > THATS IT!!! something funny....");
+            Console.WriteLine($"You guessed {GuessNum} > THATS IT!!!... something SUPER FUNNY");
             Attempts = 0;
         }
         else if (Attempts == 1 && GuessNum != SecretNumber)
@@ -34,19 +38,57 @@ void Guess()
         }
         else if (GuessNum < SecretNumber)
         {
-            Console.WriteLine($"You guessed {GuessNum} > That's too low... Try again!");
+            Console.Write($"[ Turn {Turn} ]: You guessed {GuessNum} > That's too low... Try again!!   ");
             Attempts--;
-            Console.WriteLine($"Number of guesses left: {Attempts}");
+            Turn++;
+            Console.WriteLine($"(Guesses remaining: {Attempts})");
             Guess = Console.ReadLine();
             GuessNum = int.Parse(Guess);
         }
         else
         {
-            Console.WriteLine($"You guessed {GuessNum} > That's too high... Try again!");
+            Console.Write($"[ Turn {Turn} ]: You guessed {GuessNum} > That's too high... Try again!!   ");
             Attempts--;
-            Console.WriteLine($"Number of guesses left: {Attempts}");
+            Turn++;
+            Console.WriteLine($"(Guesses remaining: {Attempts})");
             Guess = Console.ReadLine();
             GuessNum = int.Parse(Guess);
         }
     }
+}
+
+int Difficulty()
+{
+    Console.WriteLine("Select a difficulty > [  easy  |  medium  |  hard  ]");
+    string Choice = Console.ReadLine().ToLower();
+
+    int Diff = 0;
+
+    if (Choice == "easy")
+    {
+        Diff = 8;
+        return Diff;
+    }
+    else if (Choice == "medium")
+    {
+        Diff = 6;
+        return Diff;
+    }
+    else if (Choice == "hard")
+    {
+        Diff = 4;
+        return Diff;
+    }
+    else if (Choice == "cheater")
+    {
+        Diff = int.MaxValue;
+        return Diff;
+    }
+    else
+    {
+        Console.WriteLine("Please select a valid difficulty...");
+        Difficulty();
+    }
+
+    return Diff;
 }
